f3636e9f18e24368d167e101f3d94d7a95f1f4e7
Mary <pineapplecreamcheese@skiff.com>
2023-11-25 10:43:45 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,174 @@
+import { For, createSignal } from 'solid-js';
+
+import { Agent } from '@externdefs/bluesky-client/agent';
+import type { DID, RefOf } from '@externdefs/bluesky-client/atp-schema';
+
+import { formatReltime } from './intl.ts';
+
+import defaultAvatar from './assets/default-avatar.svg';
+
+const agent = new Agent({ serviceUri: 'https://api.bsky.app' });
+
+interface ProfileActivity {
+	profile: {
+		did: DID;
+		handle: string;
+		displayName?: string;
+		avatar?: string;
+	};
+	activityCount: number;
+	lastActivity: number | undefined;
+}
+
+const ACTIVITY_LIMIT = 20;
+const EMPTY_ARRAY: never[] = [];
+
+const sortProfileActivities = (a: ProfileActivity, b: ProfileActivity) => {
+	return (a.lastActivity ?? -1) - (b.lastActivity ?? -1);
+};
+
+const App = () => {
+	let controller: AbortController | undefined;
+
+	const [message, setMessage] = createSignal<string>();
+	const [activities, setActivities] = createSignal<ProfileActivity[]>(EMPTY_ARRAY);
+
+	const go = async (handle: string, signal: AbortSignal) => {
+		let follows: RefOf<'app.bsky.actor.defs#profileView'>[] = [];
+		let followCursor: string | undefined;
+
+		do {
+			setMessage(`Retrieving your follows (${follows.length} users)`);
+
+			const response = await agent.rpc.get('app.bsky.graph.getFollows', {
+				signal: signal,
+				params: {
+					actor: handle,
+					cursor: followCursor,
+				},
+			});
+
+			const data = response.data;
+
+			follows = follows.concat(data.follows);
+			followCursor = data.cursor;
+		} while (followCursor !== undefined);
+
+		let acts: ProfileActivity[] = [];
+
+		for (let idx = 0, len = follows.length; idx < len; idx++) {
+			const profile = follows[idx];
+
+			setMessage(`Retrieving @${profile.handle} (${idx + 1}/${len})`);
+
+			const response = await agent.rpc.get('app.bsky.feed.getAuthorFeed', {
+				signal: signal,
+				params: {
+					actor: profile.did,
+					limit: ACTIVITY_LIMIT + 1,
+				},
+			});
+
+			const feed = response.data.feed;
+			const now = new Date();
+
+			let activityCount = 0;
+			let lastActivity: number | undefined;
+
+			for (let idx = 0, len = feed.length; idx < len; idx++) {
+				const { post, reason } = feed[idx];
+
+				const date = new Date(reason ? reason.indexedAt : post.indexedAt);
+
+				if (
+					now.getDate() === date.getDate() &&
+					now.getMonth() === date.getMonth() &&
+					now.getFullYear() === date.getFullYear()
+				) {
+					activityCount++;
+				}
+
+				lastActivity ??= date.getTime();
+			}
+
+			acts = acts.concat({ profile: profile, activityCount: activityCount, lastActivity: lastActivity });
+			acts.sort(sortProfileActivities);
+
+			setActivities(acts);
+		}
+
+		setMessage(`Done.`);
+	};
+
+	const handleSubmit = (ev: SubmitEvent) => {
+		const formData = new FormData(ev.currentTarget as HTMLFormElement);
+		ev.preventDefault();
+
+		const handle = (formData.get('handle') as string).replace(/^@/, '');
+
+		controller?.abort();
+		controller = new AbortController();
+
+		const signal = controller.signal;
+
+		setActivities(EMPTY_ARRAY);
+		go(handle, signal).catch((err) => {
+			if (signal.aborted) {
+				return;
+			}
+
+			setMessage(`Oops, an error occured. ${err}`);
+		});
+	};
+
+	return (
+		<div>
+			<h3>Bluesky quiet posters</h3>
+			<p>List of your follows sorted by least active</p>
+
+			<form onSubmit={handleSubmit} class="input-form">
+				<input
+					type="text"
+					name="handle"
+					required
+					placeholder="example.bsky.social"
+					pattern="@?([a-zA-Z0-9\\-]+(?:\\.[a-zA-Z0-9\\-]+)*(?:\\.[a-zA-Z]+))|did:[a-z]+:[a-zA-Z0-9._\\-]+"
+					title="Bluesky handle or DID"
+				/>
+
+				<button type="submit">Go!</button>
+			</form>
+
+			<hr />
+
+			<div class="message-info">{message()}</div>
+
+			<div class="profile-list">
+				<For each={activities()}>
+					{({ profile, lastActivity }) => {
+						const url = `https://bsky.app/profile/${profile.did}`;
+
+						return (
+							<a href={url} target="_blank" class="profile">
+								<img src={/* @once */ profile.avatar || defaultAvatar} class="profile-avatar" />
+
+								<div class="profile-info">
+									<p class="profile-name">{/* @once */ profile.displayName || `@${profile.handle}`}</p>
+									<div class="profile-subinfo">
+										<span class="profile-handle">@{/* @once */ profile.handle}</span>
+										<span class="dot">·</span>
+										<span class="profile-activity">
+											{/* @once */ lastActivity !== undefined ? formatReltime(lastActivity) : 'no activity'}
+										</span>
+									</div>
+								</div>
+							</a>
+						);
+					}}
+				</For>
+			</div>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+
+import './style.css';
+import App from './App.tsx';
+
+render(() => <App />, document.getElementById('root')!);
