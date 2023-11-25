86b32caf76594c12ec5d265e110eb9b0b7088023
Mary <pineapplecreamcheese@skiff.com>
2023-11-25 10:46:50 +0700

    chore: add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ -n $(git status --porcelain) ]]; then
+	echo 'Working directory is not clean'
+	git status --short
+	exit 1
+fi
+
+GIT_COMMIT=$(git rev-parse HEAD)
+
+rsync -aHAX --delete --exclude=.git --exclude=.nojekyll dist/ deploy/
+touch deploy/.nojekyll
+
+git -C deploy/ add .
+git -C deploy/ commit -m "deploy: ${GIT_COMMIT}"
+git -C deploy/ push
